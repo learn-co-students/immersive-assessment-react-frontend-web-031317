de3c7927a77927c87a23283c150f05e627c7bbd6
Naz Khan <nazmuskhan@yahoo.com>
2017-05-26 11:01:05 -0400

    Done.

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,38 +1,56 @@
 import React from 'react'
+import Transaction from './Transaction'
 
-const TransactionsList = () => {
+const TransactionsList = (props) => {
+  if (props.transactions) {
+    const results = props.transactions.filter(function(term){
+      if (props.searchTerm === ""){
+        return props.transactions
+      }else{
+        return term.description[0].toLowerCase() === props.searchTerm[0].toLowerCase()
+      }
+    })
 
-  return (
-    <table className="ui celled striped padded table">
-      <tbody>
-        <tr>
-          <th>
-            <h3 className="ui center aligned header">
-              Posted At
-            </h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">
-              Description
-            </h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">
-              Category
-            </h3>
-          </th>
-          <th>
-            <h3 className="ui center aligned header">
-              Amount
-            </h3>
-          </th>
-        </tr>
+    const show = results.map(function(el){
+      return <Transaction result={el}/>
+    })
 
-        {"... your code here..."}
+    // console.log(results)
+    return (
+      <table className="ui celled striped padded table">
+        <tbody>
+          <tr>
+            <th>
+              <h3 className="ui center aligned header">
+                Posted At
+              </h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">
+                Description
+              </h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">
+                Category
+              </h3>
+            </th>
+            <th>
+              <h3 className="ui center aligned header">
+                Amount
+              </h3>
+            </th>
+          </tr>
 
-      </tbody>
-    </table>
-  )
+          {show}
+
+        </tbody>
+      </table>
+    )
+  }else{
+    return null
+  }
+
 }
 
 export default TransactionsList
