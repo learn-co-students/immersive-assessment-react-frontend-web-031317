42342fc8482e4e592f43df6cc587fc813a822bb5
github username <github email address>
2017-05-26 11:01:14 -0400

    Done.

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,14 +1,34 @@
 import React from 'react'
 
-const Transaction = () => {
-  return (
+const Transaction = (props) => {
+
+  if (props.searchTerm != ''){
+    if (props.transaction.description.toLowerCase().includes(props.searchTerm) || props.transaction.category.toLowerCase().includes(props.searchTerm)){
+      return (
+        <tr>
+          <td>{props.transaction.posted_at}</td>
+          <td>{props.transaction.description}</td>
+          <td>{props.transaction.category}</td>
+          <td>{props.transaction.amount}</td>
+        </tr>
+      )
+    } else {
+      return null
+    }
+  }
+
+  return(
     <tr>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
-      <td>{"...your code here"}</td>
+      <td>{props.transaction.posted_at}</td>
+      <td>{props.transaction.description}</td>
+      <td>{props.transaction.category}</td>
+      <td>{props.transaction.amount}</td>
     </tr>
   )
+
 }
 
 export default Transaction
+
+// this code definitely needs refactoring!!
+// doesn't work if user types any upper case letters :(
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
-const TransactionsList = () => {
+import Transaction from './Transaction'
+
+const TransactionsList = (props) => {
 
   return (
     <table className="ui celled striped padded table">
@@ -28,7 +30,7 @@ const TransactionsList = () => {
           </th>
         </tr>
 
-        {"... your code here..."}
+        {props.transactions.map(transaction => < Transaction transaction={transaction} searchTerm={props.searchTerm} />)}
 
       </tbody>
     </table>
